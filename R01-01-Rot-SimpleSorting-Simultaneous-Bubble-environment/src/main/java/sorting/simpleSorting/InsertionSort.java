130e4c1f8ca3afa12acc509761caf4a2f63c08bb
Amintas Victor <amintas.pereira@ccc.ufcg.edu.br>
2019-03-21 09:46:52 -0300

    Update InsertionSort.java

--- R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -14,7 +14,7 @@ public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	public void sort(T[] array, int leftIndex, int rightIndex) {
 		for (int i = leftIndex + 1; i <= rightIndex; i++) {
 			int j = i - 1;
-			while (j>= 0 && array[j].compareTo(array[j + 1]) == 1) {
+			while (j>= 0 && array[j].compareTo(array[j + 1]) > 0) {
 				util.Util.swap(array, j, j+1);
 				j--;
 			}
